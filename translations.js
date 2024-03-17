236b7ae8b1c747982c83aaaef3db90f76ddcf79e
Ramiro Neto <>
2024-03-17 12:33:07 -0300

    first commit

---
+++ translations.js
@@ -0,0 +1,225 @@
+const translations = { 
+    "pt-BR": {
+      "titulo": "Veja se sua profissão será substituída por IA",
+      "placeholder": "Digite sua profissão",
+      "botao": "Descobrir",
+      "mensagem": "Sua profissão será substituída por IA!"
+    },
+    "en": {
+      "titulo": "See if your profession will be replaced by AI",
+      "placeholder": "Enter your profession",
+      "botao": "Discover",
+      "mensagem": "Your profession will be replaced by AI!"
+    },
+    "es": {
+      "titulo": "Vea si su profesión será reemplazada por la IA",
+      "placeholder": "Ingrese su profesión",
+      "botao": "Descubrir",
+      "mensagem": "¡Su profesión será reemplazada por la IA!"
+    },
+    "hi": {
+      "titulo": "देखें कि आपका पेशा क्या एआई द्वारा प्रतिस्थापित होगा",
+      "placeholder": "अपना पेशा लिखें",
+      "botao": "खोजें",
+      "mensagem": "आपका पेशा एआई द्वारा प्रतिस्थापित होगा!"
+    },
+    "zh": {
+      "titulo": "查看您的职业是否将被AI取代",
+      "placeholder": "输入您的职业",
+      "botao": "发现",
+      "mensagem": "您的职业将被AI取代!"
+    },
+    "bn": {
+      "titulo": "আপনার পেশা কি এআই দ্বারা প্রতিস্থাপিত হবে তা দেখুন",
+      "placeholder": "আপনার পেশা লিখুন",
+      "botao": "আবিষ্কার করুন",
+      "mensagem": "আপনার পেশা এআই দ্বারা প্রতিস্থাপিত হবে!"
+    },
+    "ru": {
+      "titulo": "Посмотрите, будет ли ваша профессия заменена искусственным интеллектом",
+      "placeholder": "Введите свою профессию",
+      "botao": "Обнаружить",
+      "mensagem": "Ваша профессия будет заменена искусственным интеллектом!"
+    },
+    "ja": {
+      "titulo": "あなたの職業がAIに置き換えられるかどうかを確認してください",
+      "placeholder": "職業を入力してください",
+      "botao": "発見する",
+      "mensagem": "あなたの職業はAIに置き換えられます！"
+    },
+    "pt-PT": {
+      "titulo": "Veja se a sua profissão será substituída por IA",
+      "placeholder": "Digite a sua profissão",
+      "botao": "Descobrir",
+      "mensagem": "A sua profissão será substituída por IA!"
+    },
+    "de": {
+      "titulo": "Sehen Sie, ob Ihr Beruf durch KI ersetzt wird",
+      "placeholder": "Geben Sie Ihren Beruf ein",
+      "botao": "Entdecken",
+      "mensagem": "Ihr Beruf wird durch KI ersetzt!"
+    },
+    "ko": {
+      "titulo": "귀하의 직업이 AI에 의해 대체될지 확인하십시오",
+      "placeholder": "직업을 입력하세요",
+      "botao": "발견",
+      "mensagem": "귀하의 직업은 AI에 의해 대체됩니다!"
+    },
+    "fr": {
+      "titulo": "Voyez si votre profession sera remplacée par l'IA",
+      "placeholder": "Entrez votre profession",
+      "botao": "Découvrir",
+      "mensagem": "Votre profession sera remplacée par l'IA!"
+    },
+    "tr": {
+      "titulo": "Mesleğinizin yapay zeka tarafından değiştirilip değiştirilmeyeceğini görün",
+      "placeholder": "Mesleğinizi girin",
+      "botao": "Keşfedin",
+      "mensagem": "Mesleğiniz yapay zeka tarafından değiştirilecek!"
+    },
+    "vi": {
+      "titulo": "Xem xem công việc của bạn sẽ được thay thế bằng trí tuệ nhân tạo",
+      "placeholder": "Nhập nghề nghiệp của bạn",
+      "botao": "Khám phá",
+      "mensagem": "Công việc của bạn sẽ được thay thế bằng trí tuệ nhân tạo!"
+    },
+    "it": {
+      "titulo": "Scopri se la tua professione sarà sostituita dall'IA",
+      "placeholder": "Inserisci la tua professione",
+      "botao": "Scoprire",
+      "mensagem": "La tua professione sarà sostituita dall'IA!"
+    },
+    "ta": {
+      "titulo": "உங்கள் பொழுதுபோக்கு ஏதோனுடைய வேலைக்கு ஏஆய் மூலம் மாறும் அதை பார்க்கவும்",
+      "placeholder": "உங்கள் பொழுதுபோக்கு உங்கள் பதவி உள்ளிடவும்",
+      "botao": "கண்டறி",
+      "mensagem": "உங்கள் பொழுதுபோக்கு ஏஆய் மூலம் மாறும்!"
+    },
+    "ur": {
+      "titulo": "دیکھیں کہ آپ کا پیشہ اے آئی کی طرف سے کیا بدل دیا جائے گا",
+      "placeholder": "اپنا پیشہ لکھیں",
+      "botao": "دریافت کریں",
+      "mensagem": "آپ کا پیشہ اے آئی کی طرف سے بدل دیا جائے گا!"
+    },
+    "th": {
+      "titulo": "ดูว่าอาชีพของคุณจะถูกแทนที่ด้วย AI หรือไม่",
+      "placeholder": "ป้อนอาชีพของคุณ",
+      "botao": "ค้นพบ",
+      "mensagem": "อาชีพของคุณจะถูกแทนที่ด้วย AI!"
+    },
+    "ar": {
+      "titulo": "انظر إذا كانت مهنتك ستستبدل بواسطة الذكاء الاصطناعي",
+      "placeholder": "أدخل مهنتك",
+      "botao": "اكتشاف",
+      "mensagem": "سيتم استبدال مهنتك بواسطة الذكاء الاصطناعي!"
+    },
+    "pl": {
+      "titulo": "Sprawdź, czy twoja profesja zostanie zastąpiona przez AI",
+      "placeholder": "Wprowadź swoją profesję",
+      "botao": "Odkryć",
+      "mensagem": "Twoja profesja zostanie zastąpiona przez AI!"
+    },
+    "nl": {
+      "titulo": "Zie of uw beroep zal worden vervangen door AI",
+      "placeholder": "Voer uw beroep in",
+      "botao": "Ontdekken",
+      "mensagem": "Uw beroep zal worden vervangen door AI!"
+    },
+    "fa": {
+      "titulo": "ببینید آیا حرفه شما توسط هوش مصنوعی جایگزین خواهد شد یا خیر",
+      "placeholder": "حرفه خود را وارد کنید",
+      "botao": "کشف",
+      "mensagem": "حرفه شما توسط هوش مصنوعی جایگزین خواهد شد!"
+    },
+    "uk": {
+      "titulo": "Перевірте, чи буде ваша професія замінена штучним інтелектом",
+      "placeholder": "Введіть свою професію",
+      "botao": "Відкрити",
+      "mensagem": "Ваша професія буде замінена штучним інтелектом!"
+    },
+    "ro": {
+      "titulo": "Vedeți dacă profesia dvs. va fi înlocuită de IA",
+      "placeholder": "Introduceți profesia dvs.",
+      "botao": "Descoperi",
+      "mensagem": "Profesia dvs. va fi înlocuită de IA!"
+    },
+    "hu": {
+      "titulo": "Nézd meg, hogy a szakmád helyettesítve lesz-e AI-val",
+      "placeholder": "Írd be a szakmád",
+      "botao": "Felfedez",
+      "mensagem": "A szakmád helyettesítve lesz AI-val!"
+    },
+    "sv": {
+      "titulo": "Se om din yrke kommer att ersättas av AI",
+      "placeholder": "Ange ditt yrke",
+      "botao": "Upptäck",
+      "mensagem": "Ditt yrke kommer att ersättas av AI!"
+    },
+    "el": {
+      "titulo": "Δείτε αν η επαγγελματική σας κατηγορία θα αντικατασταθεί από την AI",
+      "placeholder": "Εισαγάγετε το επάγγελμά σας",
+      "botao": "Ανακαλύψτε",
+      "mensagem": "Το επάγγελμά σας θα αντικατασταθεί από την AI!"
+    },
+    "cs": {
+      "titulo": "Podívejte se, zda bude váš obor nahrazen AI",
+      "placeholder": "Zadejte svůj obor",
+      "botao": "Objevit",
+      "mensagem": "Váš obor bude nahrazen AI!"
+    },
+    "fi": {
+      "titulo": "Katso, korvataanko ammattisi tekoälyllä",
+      "placeholder": "Kirjoita ammattisi",
+      "botao": "Tutkia",
+      "mensagem": "Ammattisi korvataan tekoälyllä!"
+    },
+    "no": {
+      "titulo": "Se om yrket ditt vil bli erstattet av AI",
+      "placeholder": "Skriv inn yrket ditt",
+      "botao": "Oppdag",
+      "mensagem": "Yrket ditt vil bli erstattet av AI!"
+    },
+    "da": {
+      "titulo": "Se om dit erhverv vil blive erstattet af AI",
+      "placeholder": "Indtast dit erhverv",
+      "botao": "Opdag",
+      "mensagem": "Dit erhverv vil blive erstattet af AI!"
+    },
+    "sk": {
+      "titulo": "Pozrite sa, či bude vaša profesia nahradená AI",
+      "placeholder": "Zadajte svoju profesiu",
+      "botao": "Objaviť",
+      "mensagem": "Vaša profesia bude nahradená AI!"
+    },
+    "ms": {
+      "titulo": "Lihat jika profesion anda akan digantikan oleh AI",
+      "placeholder": "Masukkan profesion anda",
+      "botao": "Temui",
+      "mensagem": "Profession anda akan digantikan oleh AI!"
+    },
+    "he": {
+      "titulo": "ראה האם המקצוע שלך יוחלף על ידי AI",
+      "placeholder": "הזן את המקצוע שלך",
+      "botao": "לגלות",
+      "mensagem": "המקצוע שלך יוחלף על ידי AI!"
+    },
+    "tl": {
+      "titulo": "Tingnan kung ang iyong propesyon ay mapapalitan ng AI",
+      "placeholder": "Ipasok ang iyong propesyon",
+      "botao": "Alamin",
+      "mensagem": "Ang iyong propesyon ay mapapalitan ng AI!"
+    },
+    "id": {
+      "titulo": "Lihat apakah profesi Anda akan digantikan oleh AI",
+      "placeholder": "Masukkan profesi Anda",
+      "botao": "Temukan",
+      "mensagem": "Profesi Anda akan digantikan oleh AI!"
+    },
+    "et": {
+      "titulo": "Vaadake, kas teie ametikoht asendatakse AI-ga",
+      "placeholder": "Sisestage oma amet",
+      "botao": "Avastage",
+      "mensagem": "Teie ametikoht asendatakse AI-ga!"
+    }
+  };
+  
